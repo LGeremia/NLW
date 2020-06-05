ddfad25d8b49a5e115142d89ac6b8609053db60b
Leonardo Geremia <leonardogeremisc@gmail.com>
2020-06-04 21:41:06 -0300

    Criando html da página de cadastro dos Pontos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Routes from './router';
 
 function App() {
   return (
-    <h1>teste!</h1>
+    <Routes />
   );
 }
 
