20dcace4ad25dae5ea11dcff2239924f51956ecf
Leonardo Geremia <leonardogeremisc@gmail.com>
2020-06-04 21:50:35 -0300

    Integrando mapa ao sistema

--- web/src/Pages/CreatePoint/index.tsx
+++ web/src/Pages/CreatePoint/index.tsx
@@ -3,6 +3,7 @@ import './styles.css';
 import logo from '../../assets/logo.svg';
 import {Link} from 'react-router-dom';
 import {FiArrowLeft} from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet'
 
 const CreatePoint = ()  =>{
     return (
@@ -42,6 +43,13 @@ const CreatePoint = ()  =>{
                         <h2>Endereço:</h2>
                         <span>Selecione o endereço no mapa:</span>
                     </legend>
+                    <Map center={[-26.9932379, -51.1689972]} zoom = {15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-26.9932379, -51.1689972]}/>
+                    </Map>
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="UF">Estado:</label>
